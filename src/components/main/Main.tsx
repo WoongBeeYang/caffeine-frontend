0c7eee7d98b734dee2cc66fd0d09f9a9f732cec3
WoongBeeYang <107452925+WoongBeeYang@users.noreply.github.com>
2022-09-22 16:52:00 +0900

    Update Main.tsx

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -59,7 +59,7 @@ const Main = () => {
         className="hidden basis-2/5 border-r-[1px] bg-white md:flex"
       >
         <div className="border-b-[1px] px-4 py-4 text-left text-3xl font-bold text-zinc-800 md:border-b-0 md:px-6 md:py-6 lg:mt-40 lg:px-10 lg:py-7 lg:text-center">
-          Good {getTimeWord()} ☕
+            하이하이 웅비하이
         </div>
       </div>
       <div id="rightDiv" className="basis-3/5">
